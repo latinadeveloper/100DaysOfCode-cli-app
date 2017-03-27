dca46e89307ca55fdc04458de03d9b893226819d
Isis Tejeda <isis@isistejeda.com>
2017-03-27 11:53:44 -0500

    modified cli menu item, to work on first method there

--- lib/days_of_code/cli.rb
+++ lib/days_of_code/cli.rb
@@ -1,13 +1,20 @@
 class DaysOfCode::CLI
 
   def call
-    puts "test"
+    puts "Menu"
+    puts "Welcome to 100 Days Of Code Stats"
+    puts "Enter number for selection"
+    puts "1. For the latest 15 tweets"
 
+    input = get.strip
   end
 
-  def start
-    puts 'get tweets'
-    # input = gets.strip
+  def menu
+    case input
+    when 1
+      #last 15 tweets
+    when 2
+      #exit
 
   end
 
