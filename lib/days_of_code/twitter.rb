d53cce19a0ff5333d68e07ebaa59230206596413
Isis Tejeda <isis@isistejeda.com>
2017-03-27 12:14:10 -0500

    created_tweet method made

--- lib/days_of_code/twitter.rb
+++ lib/days_of_code/twitter.rb
@@ -1,10 +1,8 @@
 # from twitter documentation
-#https://dev.twitter.com/oauth/overview/single-user   how to connect as a single user to twitter
-
 class DaysOfCode::Twitter
-attr_reader :consumer
+  attr_reader :consumer
 
-#https://apps.twitter.com/
+  #https://apps.twitter.com/
   def initialize(consumer_key = CONSUMER_KEY, consumer_secret = CONSUMER_SECRET)  #defaults from doNotCommit.rb
     @consumer = OAuth::Consumer.new(consumer_key, consumer_secret, { :site => "https://api.twitter.com", :scheme => :header })
     #consumer is to read only
@@ -15,9 +13,18 @@ attr_reader :consumer
     response = consumer.request(:get, "https://api.twitter.com/1.1/search/tweets.json?q=%23100DaysOfCode")
     result = JSON.parse(response.body) # <-- response back from twitter
   end
+
+  def create_tweet
+    tweet = twitter.new.get_twitter[]"statuses"]
+    Tweets.create_from_tweet(tweet_array)
+  end
+
+
 end
 
 
 
 
-  #https://dev.twitter.com/rest/public/search   build a query
+
+#https://dev.twitter.com/oauth/overview/single-user   how to connect as a single user to twitter
+#https://dev.twitter.com/rest/public/search   build a query
