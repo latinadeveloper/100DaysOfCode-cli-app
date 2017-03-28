e67c987b843d2aeedbc1510a39fb40da2c1b647e
Isis Tejeda <isis@isistejeda.com>
2017-03-28 09:09:10 -0500

    yes or no menu works

--- lib/days_of_code/cli.rb
+++ lib/days_of_code/cli.rb
@@ -8,38 +8,34 @@ class DaysOfCode::CLI
 
   def menu
 
-loop do
-    puts "----- Menu-----"
-    puts "Welcome to 100 Days Of Code Stats"
-    puts "Enter number for selection"
-    puts "   "
-    puts "1. For the latest 15 tweets"
-    puts "2. Request a number of recent users"
-    print ":".colorize(:color => :black, :background => :yellow, :mode => :blink)
-      input = gets.to_i
+    loop do
+      puts "----- Menu-----"
+      puts "Welcome to 100 Days Of Code Stats"
+      puts "Enter number for selection"
+      puts "   "
+      puts "1. For the latest 15 tweets"
+      puts "2. Request a number of recent users"
+      print ":".colorize(:color => :black, :background => :yellow, :mode => :blink)
+        input = gets.to_i
 
 
-    case input
-    when 1
-      latest_15_tweets
-    when 2
-      puts "How many recent users do you want to see from 1 to 15"
-      request = gets.to_i
-      recent_users(request)
-    end
-
-    puts " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    puts " Do you want another option?"
-    input = gets.downcase
-    if input == "no" || input == "n"
-      return
-    else
-      menu
-    end
-break 
+      case input
+      when 1
+        latest_15_tweets
+      when 2
+        puts "How many recent users do you want to see from 1 to 15"
+        request = gets.to_i
+        recent_users(request)
+      end
 
 
-  end
+      if !yes_or_no
+        return # ends
+      end
+
+
+    end # end of loop
+  end # end menu
 
 
 
@@ -72,6 +68,22 @@ break
   end
 
 
+  def yes_or_no
+    puts " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    loop do
+      puts " Do you want another option?"
+      input = gets.downcase.strip
+      if input == "no" || input == "n"
+        return false
+
+      elsif  input == "yes" || input == "y"
+        return true
+
+      else
+        puts "Choose yes or no"
+      end
+    end
+  end
 
 
 
