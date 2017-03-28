d5aad27c157a618bde28c60187fa8939e9603a81
Isis Tejeda <isis@isistejeda.com>
2017-03-27 21:59:11 -0500

    recent users passes a number

--- lib/days_of_code/cli.rb
+++ lib/days_of_code/cli.rb
@@ -1,20 +1,24 @@
 class DaysOfCode::CLI
 
+
   def call
     puts "Menu"
     puts "Welcome to 100 Days Of Code Stats"
     puts "Enter number for selection"
     puts "1. For the latest 15 tweets"
-create_tweet
-latest_15_tweets
+    puts "2. Request a number of recent users"
+    input = gets.to_i
+    menu(input)
   end
 
-  def menu
+
+  def menu(input)
     case input
     when 1
       latest_15_tweets
     when 2
-      return
+      puts "how many recent users do you want to see from 1 to 15"
+     request = gets.to_i
     end
   end
 
@@ -32,12 +36,14 @@ latest_15_tweets
      end
   end
 
-  def recent_users
-    abc_array = DaysOfCode::Tweets.all[0..9].collect do |tweet|
-      tweet.screen_name
+  def recent_users(request)
+      puts "                   Recent users                 ".colorize(:color => :black, :background => :magenta, :mode => :bold)
+    abc_array = DaysOfCode::Tweets.all[0...request].collect do |tweet|
+      tweet.screen_name.downcase
     end
 
-    abc_array.each do |abc|
+
+    abc_array.sort.each do |abc|
       puts abc
       end
   end
